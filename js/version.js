07a27d9c2d4de838d6801dd0be1aa86e81314bd0
Sujee Maniyam <sujee@sujee.dev>
2026-09-24 15:50:31 -0700

    text cleanup

--- js/version.js
+++ js/version.js
@@ -8,7 +8,7 @@
 
     // Current app version. Bump this when shipping user-visible changes;
     // snake.html cache-busters (?v=) track it so deploys bust stale assets.
-    const APP_VERSION = '9';
+    const APP_VERSION = '10';
 
     function displayText() {
         return `v${APP_VERSION}`;
